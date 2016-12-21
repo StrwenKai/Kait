6a9afaf482f85615c408cd24216ede636d0476dd
StrWK <481433863@qq.com>
2016-12-21 17:57:35 +0800

    制作初始界面视图LitView

---
+++ reminders/src/main/java/com/example/reminders/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.reminders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//实现备忘录的增删改除
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
